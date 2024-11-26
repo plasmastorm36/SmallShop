f12e18ec15fc8cd8f50d53cc89247ba7e064ecc7
plasmastorm36 <plasmastormfire@gmail.com>
2024-11-26 13:04:15 -0500

    Added Order repository to find user orders

---
+++ main/src/main/java/com/shop/repository/OrderRepository.java
@@ -0,0 +1,22 @@
+package com.shop.repository;
+
+import com.shop.main.entity.Order;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Order repository to find user orders
+ * @author Noah Rouse
+ * @email noahrouse36@gmail.com
+ */
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+   
+   @Query("SELECT o FROM Order o WHERE o.user.id = :user_id")
+   public List<Order> findUserOrders(final long user_id);
+}
