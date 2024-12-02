b1fe5c3f7bf7590ffecc2c1ca7d1c618d0c15f78
plasmastorm36 <plasmastormfire@gmail.com>
2024-12-02 12:02:03 -0500

    Added a detail service to connect logins to spring boot security

---
+++ main/src/main/java/com/shop/main/Service/CustomerDetailsService.java
@@ -0,0 +1,37 @@
+package com.shop.main.service;
+
+import com.shop.main.repository.UserRepository;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * A detail service made to connect user entity to security user class for login
+ * @author Noah Rouse
+ * @email noahrouse36@gmail.com
+ */
+@Service
+public class CustomerDetailsService implements UserDetailsService {
+   private final UserRepository repos;
+   public CustomerDetailsService(final UserRepository userRepository) {
+      this.repos = userRepository;
+   }
+
+   public UserDetails loadUserByUsername (final String userName) 
+         throws UsernameNotFoundException {
+      final Optional<com.shop.main.entity.User> user = repos.findByUserName(userName);
+
+      if (!user.isPresent()) {
+         throw new UsernameNotFoundException(String.format("User with username %s not found",
+               userName));
+      }
+
+      return User.builder().username(user.get().getUserName())
+            .password(user.get().getPassword()).roles("USER").build();
+   }
+}
