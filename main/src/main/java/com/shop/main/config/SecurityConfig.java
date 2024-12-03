9a5284366bab7c99e718c8cda5ae301e39d2945c
plasmastorm36 <plasmastormfire@gmail.com>
2024-12-02 22:32:00 -0500

    Added Login functionality

--- main/src/main/java/com/shop/main/config/SecurityConfig.java
+++ main/src/main/java/com/shop/main/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
  * @email noahrouse36@gmail.com
  */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
    private final CustomerDetailsService service;
    public SecurityConfig (final CustomerDetailsService customerDetailsService) {
---
+++ main/src/main/java/com/shop/main/controller/LoginController.java
@@ -0,0 +1,54 @@
+package com.shop.main.controller;
+
+import com.shop.main.service.LoginService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+/**
+ * Controller to handle login and default page
+ * @author Noah Rouse
+ * @email noahrouse36@gmail.com
+ */
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+   private final LoginService loginService;
+   private final UserDetailsService userDetailsService;
+   public LoginController (final LoginService loginService, UserDetailsService userDetailsService) {
+      this.loginService = loginService;
+      this.userDetailsService = userDetailsService;
+   }
+
+   /**
+    * allows a user to login
+    * @param username
+    * @param password
+    * @return
+    */
+   @PostMapping("/login")
+   public ResponseEntity<String> login (final @RequestParam String username, final @RequestParam String password) {
+      if (loginService.authenticate(username, password)) {
+         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+         
+         // Authentication object with proper roles
+         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+               userDetails, null, userDetails.getAuthorities());
+
+         SecurityContextHolder.getContext().setAuthentication(token);
+         return ResponseEntity.ok("Login Successful");
+      }
+
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid Credentials");
+   }
+}
