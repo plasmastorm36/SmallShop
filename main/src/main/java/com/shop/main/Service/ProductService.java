a08b3fbd4969ccf90375c3163066f317c5ac16f0
plasmastorm36 <plasmastormfire@gmail.com>
2024-12-02 23:02:54 -0500

    Added a repository and service layer for handling products

---
+++ main/src/main/java/com/shop/main/Service/ProductService.java
@@ -0,0 +1,60 @@
+package com.shop.main.service;
+
+import com.shop.main.entity.Product;
+import com.shop.main.repository.ProductRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Product service to handle logic details
+ * @author Noah Rouse
+ * @email noahrouse36@gmail.com
+ */
+@Service
+public class ProductService {
+   private final ProductRepository repos;
+   public ProductService (final ProductRepository productRepository) {
+      this.repos = productRepository;
+   }
+
+   public void addProduct (final Product product) {
+      repos.save(product);
+   }
+
+   public void deleteProduct (final Product product) {
+      repos.delete(product);
+   }
+
+   public List<Product> findAllProducts () {
+      return repos.findAll();
+   }
+
+   public Product findProductById (final long id) {
+      final Optional<Product> product = repos.findById(id);
+      if (product.isPresent()) {
+         return product.get();
+      }
+
+      throw new EntityNotFoundException(String.format("Product with id %d not found", id));
+   }
+
+   public List<Product> findProductsByName (final String name) {
+      return repos.findByName(name);
+   }
+
+   public void updateProductPrice (final Product product, final BigDecimal price) {
+      product.setPrice(price);
+      repos.save(product);
+   }
+
+   public void updateProductQuantity (final Product product, final int quantity) {
+      product.setQuantity(quantity);
+      repos.save(product);
+   }
+}
---
+++ main/src/main/java/com/shop/main/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.shop.main.repository;
+
+import com.shop.main.entity.Product;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository for products
+ * @author Noah Rouse
+ * @email noahrouse36@gmail.com
+ */
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+   @Query("SELECT p FROM Products WHERE p.name = :name")
+   List<Product> findByName(final String name);
+}
